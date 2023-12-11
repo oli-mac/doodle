25624c5a6b6361710235dee4d52f725f0c7f8c31
oli-mac <adumulugeta@gmail.com>
2023-12-11 02:25:29 -0900

    otp

--- lib/features/auth/pages/otp.dart
+++ lib/features/auth/pages/otp.dart
@@ -1,4 +1,11 @@
+import 'package:doodle/common/utils/constants.dart';
+import 'package:doodle/common/widgets/appstyle.dart';
+import 'package:doodle/common/widgets/height_spacer.dart';
+import 'package:doodle/common/widgets/reusable_text.dart';
+import 'package:doodle/features/todo/pages/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pinput/pinput.dart';
 
 class Otp extends StatelessWidget {
   const Otp({super.key});
@@ -6,10 +13,47 @@ class Otp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Otp'),
-        backgroundColor: Colors.transparent,
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Otp'),
+          backgroundColor: Colors.transparent,
+        ),
+        body: SafeArea(
+          child: Center(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              HeightSpacer(height: AppConstants.appHeight * 0.12),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.w),
+                  child: Image.asset(
+                    'assets/images/todo.png',
+                    width: AppConstants.appHeight * 0.9,
+                    height: 180,
+                  )),
+              HeightSpacer(height: 26),
+              ReusableText(
+                  text: "Enter your otp code",
+                  style: appstyle(18, AppConstants.appLight, FontWeight.bold)),
+              HeightSpacer(height: 26),
+              Pinput(
+                length: 6,
+                autofocus: true,
+                showCursor: true,
+                onCompleted: (value) {
+                  if (value.length == 6) {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: ((context) => HomePage())));
+                  }
+                },
+                onSubmitted: (value) {
+                  if (value.length == 6) {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: ((context) => HomePage())));
+                  }
+                },
+              )
+            ],
+          )),
+        ));
   }
 }
